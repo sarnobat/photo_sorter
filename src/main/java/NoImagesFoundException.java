6469d05a5b16a0ba56741bee6bf1e5bceeade592
Sridhar Sarnobat <ss401533@gmail.com>
2020-09-07 14:02:36 -0700

    Minor changes (untested)

--- src/main/java/NoImagesFoundException.java
+++
@@ -1,6 +0,0 @@
-
-public class NoImagesFoundException extends Exception {
-
-	private static final long serialVersionUID = -6647160297175674127L;
-
-}
--- src/main/java/com/rohidekar/photosorter/NoImagesFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.rohidekar.photosorter;
-
-
-public class NoImagesFoundException extends Exception {
-
-	private static final long serialVersionUID = -6647160297175674127L;
-
-}
